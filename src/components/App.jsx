71f4dbbeffd8fd8a5f320e9ba20630fbffe34b49
duyenle1312 <lengomyduyen@gmail.com>
2022-06-06 07:31:12 +0300

    Create components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import { CssBaseline } from '@mui/material'
+import { Route, Switch } from 'react-router-dom'
 
 const App = () => {
   return (
-    <div>App</div>
+    <>
+      <CssBaseline />
+      <main>
+        <Switch>
+          <Route exact path="/movie/:id">
+            <h1>Movie Info</h1>
+          </Route>
+          <Route exact path="/actor/:id">
+            <h1>Actor</h1>
+          </Route>
+          <Route exact path="/">
+            <h1>Movies</h1>
+          </Route>
+          <Route exact path="/profile/:id">
+            <h1>Profile</h1>
+          </Route>
+        </Switch>
+      </main>
+    </>
   )
 }
 
